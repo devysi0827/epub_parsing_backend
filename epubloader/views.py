95b4d8cc8c03dbef6807cdbb92d7837b402d8a52
devysi0827 <devysi0827@gmail.com>
2022-04-28 20:28:51 +0900

    be

--- epubloader/views.py
+++ epubloader/views.py
@@ -10,55 +10,9 @@ from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import xmltodict
 
-
-
-# @csrf_exempt
-# def getfile(request):
-#     # 각 변수 설정
-#     zipopf ="opf is null"
-#     zipcontent = "contetnt is null"
-#     zipImage = "image is null"
-
-#     # try,except
-#     try:
-#         if request.method == "POST":
-#             print("posted file")
-#             try:
-#                 client_file = request.FILES['file']
-#                 # unzip the zip file to the same directory 
-#                 with zipfile.ZipFile(client_file, 'r') as zip_ref:
-#                     ziplist = zip_ref.infolist()
-#                     print('-------------------------')
-#                     print("unzip")
-#                     for i in range(0,len(ziplist)):
-#                         selectedFile = ziplist[i]
-#                         with zip_ref.open(selectedFile,"r") as sf:
-#                             zipname = sf.name
-#                             if zipname[-3:] == 'opf':
-#                                 zipopf = sf.read() 
-#                             if zipname[-5:] == "xhtml":
-#                                 zipcontent = sf.read()
-#                             if zipname[-3:] == 'jpg':
-#                                 zipImage = sf.read()
-
-#                 print("start input dict")
-#                 temp_dict = {}
-#                 temp_dict["name"] ="opfFile"
-#                 temp_dict["spine"] = zipopf.decode('utf-8')
-#                 temp_dict["xhtml"] = zipcontent.decode('utf-8')
-#                 temp_dict['image'] = base64.encodebytes(zipImage).decode('utf-8')
-
-#                 print("end input dict")
-#                 return HttpResponse(json.dumps(temp_dict), content_type="application/json")
-
-#             except Exception as e:
-#                 return HttpResponse("error1")
-#     except :
-#         return HttpResponse("error2")
-
 @csrf_exempt
 def ImageController(request):
-     # 각 변수 설정
+    # 각 변수 설정
     zipopf ="opf is null"
 
     # try,except
